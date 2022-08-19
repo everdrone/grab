54778bfbaee6664a534370c2cc71a78cd586d16e
everdrone <giorgiotropiano@gmail.com>
2022-08-19 15:03:08 +0200

    style: rename info.go to version.go

--- scripts/standard-version-updater.js
+++ scripts/standard-version-updater.js
@@ -1,23 +1,20 @@
-module.exports.readVersion = (contents) => {
-  const re = /const\sVersion\s=\s"(.*)"/;
+module.exports.readVersion = contents => {
+  const re = /const\sVersion\s=\s"(.*)"/
 
   if (!contents.match(re)[1]) {
-    throw new Error("Could not find version.");
+    throw new Error('Could not find version.')
   }
 
-  console.log("--------------");
-  console.log(contents.match(re)[1]);
+  console.log(contents.match(re)[1])
 
-  return contents.match(re)[1];
-};
+  return contents.match(re)[1]
+}
 
 module.exports.writeVersion = (contents, version) => {
-  const re = /const\sVersion\s=\s"(.*)"/;
-  const oldVersion = contents.match(re)[1];
+  const re = /const\sVersion\s=\s"(.*)"/
+  const oldVersion = contents.match(re)[1]
 
-  console.log("--------------");
-  console.log(oldVersion, version);
-  console.log(contents.replace(oldVersion, version));
+  console.log(oldVersion, '->', version)
 
-  return contents.replace(oldVersion, version);
-};
+  return contents.replace(oldVersion, version)
+}
